e90e6b19952a47d8c7ecb77379a72dc2c013c952
nahimanajz <jazzonahimana@gmail.com>
2021-07-02 00:53:49 +0200

    deploy

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ import path from 'path'
 env.config()
 const app = express()
 app.use(cors())
-
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname,'frontend/build')))   
-app.get('*', (req, res) => { 
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html')) 
-});
-
 app.use('/api/v1', routes)
-app.get('/test',(req,res)=>res.send('hello'))
-
 
 app.listen(process.env.PORT || 5000,()=>console.log(`App is running on port ${process.env.PORT}`))
